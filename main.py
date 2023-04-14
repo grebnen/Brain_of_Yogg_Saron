6b598a9eb07cd729ade384862fcbf90966b4e620
Justice Kennedy <107448861+JusticeKennedy97@users.noreply.github.com>
2023-04-13 20:30:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,66 +3,69 @@
 # The dictionary links a room to other rooms.
 import sys
 
+# A dictionary for brain of Yogg Saron
+# The dictionary links a room to other rooms.
 rooms = {
     'Freyas sanctuary of life': {'South': 'chamber of promises'},
-    'chamber of promises': {'North': 'Freyas sancturay of life', 'East': 'thorims room'},
+    'chamber of promises': {'North': 'Freyas sanctuary of life', 'East': 'thorims room'},
     'thorims room': {'South': 'Freyas sanctuary of life', 'West': 'hodirs chamber'},
     'hodirs chamber': {'West': 'Brain of Yogg Saron'}
 }
+
 items = {
     'Freyas sanctuary of life': 'Blossom',
     'chamber of promises': 'Beacon of Communication',
-    'Hodirs Chamber': 'Ring',
-    'Thorims room': 'Thorims hammer',
+    'hodirs chamber': 'Ring',
+    'thorims room': 'Thorims hammer',
     'Brain of Yogg Saron': 'Yogg Saron'
-
 }
+
 state = 'Freyas sanctuary of life'
 inventory = []
 
+def get_new_state(direction, current_room):
+    new_room = current_room
 
-# function
-def get_new_state(state, direction):
-    new_state = state  # declaring
-    for i in rooms:  # loop
-        if i == state:  # if
-            if direction in rooms[i]:  # if
-                new_state = rooms[i][direction]  # assigning new_state
+    if direction in rooms[current_room]:
+        new_room = rooms[current_room][direction]
 
-    return new_state  # return
+    return new_room
 
+def show_status():
+    print('You are currently in', state)
+    print('Available in this room is', items[state])
+    print('You currently have', inventory)
+    print('You can move to the following rooms:', ', '.join(list(rooms[state].keys())))
 
-while 1:  # gameplay loop
-    print('You are in the ', state)  # printing state
-    if state == 'Brain of Yogg saron':
+while True:
+    show_status()
+
+    if state == 'Brain of Yogg Saron':
         print('Battling with Yogg Saron', end='')
-        for i in range(50):
-            for j in range(1000000):
-                pass
-            print(".", end='', flush=True)
         print()
         if len(inventory) == 4:
             print('You have defeated Yogg Saron - congratulations')
         else:
-            print('Yogg Saron has consumed you, - try again')
+            print('Yogg Saron has consumed you - try again')
         break
 
-    print('Available to you in this room is', items[state])
-    print('You currently have', inventory)
-    direction = input('Enter item you want OR direction to go OR exit to give up: ')  # asking user
+    direction = input('Enter item you want OR direction to go OR exit to give up: ')
+
     if direction.lower() == items[state].lower():
         if items[state] not in inventory:
             inventory.append(items[state])
         continue
-    direction = direction.capitalize()  # making first character capital remaining lower
-    if direction == 'Exit':  # if
-        sys.exit(0)  # exit function
-    if direction == 'East' or direction == 'West' or direction == 'North' or direction == 'South':  # if
-        new_state = get_new_state(state, direction)  # calling function
-        if new_state == state:  # if
-            print('A great evil is in that direction, quickly try another direction!')  # print
-        else:
-            state = new_state  # changing state value to new_state
-    else:
-        print('Invalid direction!!')  # print
 
+    direction = direction.capitalize()
+
+    if direction == 'Exit':
+        sys.exit(0)
+
+    if direction in ['East', 'West', 'North', 'South']:
+        new_state = get_new_state(direction, state)
+        if new_state == state:
+            print('A great evil is in that direction, quickly try another direction!')
+        else:
+            state = new_state
+    else:
+        print('Invalid direction!')
